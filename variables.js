9fd06622d4cd67e20f1fefcb249f1fa2ac55dba4
ubaid-syed <arooj.ubaid777@gmail.com>
2024-02-10 23:36:01 +0500

    variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+const accountId = 122334;
+let accountEmail = "ubaid@gmail.com";
+var accountPassword = "12334";
+
+accountEmail = 2;
+
+// Prefer not to use var because of issue in block scope and functional scope
+
+console.table([accountId, accountEmail, accountPassword]);
