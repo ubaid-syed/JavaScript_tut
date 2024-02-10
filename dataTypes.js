d391b3512fe9b503f397986e44362d310c20974b
ubaid-syed <arooj.ubaid777@gmail.com>
2024-02-10 23:50:34 +0500

    DataTypes

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treact all JS code as newer version
+
+// alert(3 + 3);  // we are using nodejs, not browsing
+
+console.log(3 + 3); // code readability should be high
+console.log("ubaid");
+
+let name = "ubaid";
+let age = 16;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+//string => "" and ''
+// boolean => true/false
+//null => standalone value
+//undefined =>
+// symbols => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
